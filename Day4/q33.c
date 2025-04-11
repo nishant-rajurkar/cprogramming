08b50356c9d0b4b634da67aafe2ec558ea25a0de
Nishant Rajurkar <nishantrajurkar1994@gmail.com>
2025-04-11 10:39:46 +0530

    first commit

--- Day4/q33.c
+++ Day4/q33.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("enter the value of x and y");
+    scanf("%d%d",&x,&y);
+    if(x==1 && y==1)
+    {
+        printf("It belongs to quadrant IV");
+    }
+}
--- Day4/q38.c
+++ Day4/q38.c
@@ -1,9 +1,13 @@
  #include<stdio.h>
  int main()
  {
-    int i,n;
-    for(int i=1;i<=n,i++)
-    {
-        i=
-    }
+    int n,n1,n2,n3;
+    printf("enter the value of n");
+    scanf("%d\t",&n);
+    n1=n;
+    printf("%d\t",n1);
+    n2=n*n;
+    printf("%d\t",n2);
+    n3=n*n*n;
+    printf("%d\t",n3);
  }
