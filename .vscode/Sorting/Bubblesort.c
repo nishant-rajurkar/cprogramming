5ed4a567ea13dec9f5115dfc64ddcbcd5d27ed62
Nishant Rajurkar <nishantrajurkar1994@gmail.com>
2025-05-06 11:44:23 +0530

    first commit

---
+++ .vscode/Sorting/Bubblesort.c
@@ -0,0 +1,37 @@
+void bubbleSort(int arr[], int n) 
+{
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) 
+    {
+        for (j = 0; j < n - i - 1; j++) 
+	{
+            if (arr[j] > arr[j + 1]) 
+	    {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) 
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+        printf("\n");
+    }
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array:\n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Sorted array:\n");
+    printArray(arr, n);
+    return 0;
+}
